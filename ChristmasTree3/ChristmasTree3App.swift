49dd1f0467788de5a358c06fd6f24599262cf550
VulcanCCIT <chuckcondron@chuckcondron.com>
2023-12-24 12:03:43 -0600

    Initial Commit

---
+++ ChristmasTree3/ChristmasTree3App.swift
@@ -0,0 +1,17 @@
+//
+//  ChristmasTree3App.swift
+//  ChristmasTree3
+//
+//  Created by Chuck Condron on 12/24/23.
+//
+
+import SwiftUI
+
+@main
+struct ChristmasTree3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChristmasTree3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChristmasTree3
+//
+//  Created by Chuck Condron on 12/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
